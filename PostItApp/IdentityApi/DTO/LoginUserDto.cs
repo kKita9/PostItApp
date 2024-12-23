386368e35e220983e40f21662df5b596ad3fa509
Karol Kita <kkita970@gmail.com>
2024-12-23 22:52:56 +0100

    update user controller

---
+++ PostItApp/IdentityApi/DTO/LoginUserDto.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace IdentityApi.DTO
+{
+    public class LoginUserDto
+    {
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        public string Password { get; set; }
+    }
+}
--- PostItApp/IdentityApi/DTO/RegisterUserDto.cs
+++ PostItApp/IdentityApi/DTO/RegisterUserDto.cs
@@ -6,11 +6,20 @@ namespace IdentityApi.DTO
     public class RegisterUserDto
     {
         [Required]
-        [MaxLength(50)]
-        public string Username { get; set; }
+        [EmailAddress]
+        public string Email { get; set; }
 
         [Required]
         [MaxLength(50)]
+        public string FirstName { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string LastName { get; set; }
+
+        [Required]
+        [MinLength(6)]
         public string Password { get; set; }
     }
+
 }
