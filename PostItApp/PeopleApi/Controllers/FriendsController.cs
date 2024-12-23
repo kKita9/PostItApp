bdbd1c07231fa9bb5428f4052d8b60ddfa45378c
Karol Kita <kkita970@gmail.com>
2024-12-23 23:24:45 +0100

    update friend controller

--- PostItApp/PeopleApi/Controllers/FriendsController.cs
+++ PostItApp/PeopleApi/Controllers/FriendsController.cs
@@ -1,73 +1,133 @@
-﻿//using DataAccess.Data;
-//using DataAccess.Models;
-//using Microsoft.AspNetCore.Mvc;
-//using PeopleApi.Dto;
+﻿using DataAccess.Data;
+using DataAccess.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using PeopleApi.Dto;
 
-//namespace PeopleApi.Controllers
-//{
-//    [Route("api/[controller]")]
-//    [ApiController]
-//    public class FriendsController : ControllerBase
-//    {
-//        private readonly ApplicationDbContext _context;
+namespace PeopleApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize]
+    public class FriendsController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
 
-//        public FriendsController(ApplicationDbContext context)
-//        {
-//            _context = context;
-//        }
+        public FriendsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
 
-//        // get all friends 
-//        [HttpGet]
-//        public IActionResult GetFriends()
-//        {
-//            var friends = _context.Friends
-//                .Select(f => new FriendDto
-//                {
-//                    Id = f.Id,
-//                    Name = f.Name,
-//                    Email = f.Email
-//                })
-//                .ToList();
+        [HttpGet("list")]
+        public IActionResult GetFriends()
+        {
+            var userId = int.Parse(User.FindFirst("id").Value);
 
-//            return Ok(friends);
-//        }
+            var friends = _context.Users
+                .Include(u => u.Friends)
+                .FirstOrDefault(u => u.Id == userId)?
+                .Friends
+                .Select(f => new FriendDto
+                {
+                    Id = f.Id,
+                    FirstName = f.FirstName,
+                    LastName = f.LastName,
+                    Email = f.Email
+                })
+                .ToList();
 
-//        // add new friend 
-//        [HttpPost]
-//        public IActionResult AddFriend([FromBody] CreateFriendDto createFriendDto)
-//        {
-//            var friend = new Friend
-//            {
-//                Name = createFriendDto.Name,
-//                Email = createFriendDto.Email
-//            };
+            if (friends == null || !friends.Any())
+            {
+                return Ok(new { Message = "No friends found." });
+            }
 
-//            _context.Friends.Add(friend);
-//            _context.SaveChanges();
+            return Ok(friends);
+        }
 
-//            var friendDto = new FriendDto
-//            {
-//                Id = friend.Id,
-//                Name = friend.Name,
-//                Email = friend.Email
-//            };
+        [HttpGet("suggested")]
+        public IActionResult GetSuggestedFriends()
+        {
+            var userId = int.Parse(User.FindFirst("id").Value);
 
-//            return CreatedAtAction(nameof(GetFriends), new { id = friend.Id }, friendDto);
-//        }
+            var user = _context.Users
+                .Include(u => u.Friends)
+                .FirstOrDefault(u => u.Id == userId);
 
-//        // remove friend 
-//        [HttpDelete("{id}")]
-//        public IActionResult DeleteFriend(int id)
-//        {
-//            var friend = _context.Friends.FirstOrDefault(f => f.Id == id);
-//            if (friend == null)
-//            {
-//                return NotFound();
-//            }
+            if (user == null)
+            {
+                return NotFound("User not found.");
+            }
 
-//            _context.Friends.Remove(friend);
-//            _context.SaveChanges();
-//            return NoContent();
-//        }
-//    }
-//}
+            var friendsIds = user.Friends.Select(f => f.Id).ToHashSet();
+            friendsIds.Add(userId);
+
+            var suggestedFriends = _context.Users
+                .Where(u => !friendsIds.Contains(u.Id))
+                .Select(u => new FriendDto
+                {
+                    Id = u.Id,
+                    FirstName = u.FirstName,
+                    LastName = u.LastName,
+                    Email = u.Email
+                })
+                .ToList();
+
+            return Ok(suggestedFriends);
+        }
+
+        [HttpPost("add/{friendId}")]
+        public IActionResult AddFriend(int friendId)
+        {
+            var userId = int.Parse(User.FindFirst("id").Value); 
+
+            var user = _context.Users
+                .Include(u => u.Friends)
+                .FirstOrDefault(u => u.Id == userId);
+
+            var friend = _context.Users.FirstOrDefault(u => u.Id == friendId);
+
+            if (user == null || friend == null)
+            {
+                return NotFound("User or friend not found.");
+            }
+
+            if (user.Friends.Any(f => f.Id == friendId))
+            {
+                return BadRequest("This user is already your friend.");
+            }
+
+            user.Friends.Add(friend);
+            _context.SaveChanges();
+
+            return Ok("Friend added successfully.");
+        }
+
+        [HttpDelete("remove/{friendId}")]
+        public IActionResult RemoveFriend(int friendId)
+        {
+            var userId = int.Parse(User.FindFirst("id").Value); 
+
+            var user = _context.Users
+                .Include(u => u.Friends)
+                .FirstOrDefault(u => u.Id == userId);
+
+            if (user == null)
+            {
+                return NotFound("User not found.");
+            }
+
+            var friend = user.Friends.FirstOrDefault(f => f.Id == friendId);
+
+            if (friend == null)
+            {
+                return BadRequest("This user is not your friend.");
+            }
+
+            user.Friends.Remove(friend);
+            _context.SaveChanges();
+
+            return Ok("Friend removed successfully.");
+        }
+    }
+}
--- PostItApp/PeopleApi/Dto/CreateFriendDto.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace PeopleApi.Dto
-{
-    public class CreateFriendDto
-    {
-        public string Name { get; set; }
-        public string Email { get; set; }
-    }
-}
